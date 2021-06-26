725fdb75df0f723b44180e1f45813134e0da161e
kulloveth <kulloveth@gmail.com>
2021-06-26 09:59:54 +0100

    new files with firebase integration

---
+++ wildfire/android/app/src/main/kotlin/com/reskillamericans/wildfire/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.reskillamericans.wildfire
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ wildfire/lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final fireUpdate= FirebaseFirestore.instance
+      .collection('fire-updates');
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+          StreamBuilder<QuerySnapshot>(
+            stream: fireUpdate.snapshots(),
+            builder: (context,snapshots){
+              snapshots.data?.docs.forEach((element) {
+                print("update ${element.data()}");
+              });
+
+              return Container();
+            },
+          )
+          ],
+        ),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
