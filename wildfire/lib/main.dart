a9f522ed2f204ed6819379a15089ffc11d0eff10
RollieDev <rolandoaniel@gmail.com>
2021-06-30 17:55:03 -0700

    Initial Commit

--- wildfire/lib/main.dart
+++ wildfire/lib/main.dart
@@ -2,10 +2,11 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(MyApp());
+  runApp(WildfireApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -32,11 +33,9 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final fireUpdate= FirebaseFirestore.instance
-      .collection('fire-updates');
+  final fireUpdate = FirebaseFirestore.instance.collection('fire-updates');
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
@@ -45,14 +44,14 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-          StreamBuilder<QuerySnapshot>(
-            stream: fireUpdate.snapshots(),
-            builder: (context,snapshots){
-              snapshots.data?.docs.forEach((element) {
-                print("update ${element.data()}");
-              });
+            StreamBuilder<QuerySnapshot>(
+              stream: fireUpdate.snapshots(),
+              builder: (context, snapshots) {
+                snapshots.data?.docs.forEach((element) {
+                  print("update ${element.data()}");
+                });
 
-              return Container();
+                     return Container();
             },
           )
           ],
@@ -61,3 +60,85 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+
+ @override
+  _WildfireAppState createState() => _WildfireAppState();
+
+class _WildfireAppState extends State<WildfireApp> {
+  late String name, phoneNumber, location, detail, time;
+
+
+    CollectionReference fireUpdate = FirebaseFirestore.instance.collection('fire-updates');
+
+
+  getName(name) {
+    this.name = name;
+  }
+
+  getPhoneNumber(phone-number) {
+    this.phoneNumber = phoneNumber;
+  }
+
+  getLocation(location) {
+    this.location = location;
+  }
+
+  getDetail(detail) {
+    this.detail = detail;
+  }
+
+  getTime(time) {
+    this.time = time;
+  }
+
+  
+void readData(){
+    FirebaseFirestore.instance.collection("fire-updates").doc().get();
+      print("DATA ['name']}");
+      print("DATA ['phone-number']}");
+      print("DATA ['location']}");
+      print("DATA ['detail']}");
+      print("DATA ['time']}");
+    }
+ }
+
+   Map<String,dynamic> toJson(){
+      return{
+      "name": name,
+      "phone-number": phoneNumber,
+      "location": location,
+      "detail": detail,
+      "time": time,
+    };
+  
+  }
+
+void createData(){
+    FirebaseFirestore.instance.collection('fire-updates').doc().set({
+      'name': 'John Doe',
+      'phone-number': '2060000000',
+      'location': 'Northwest',
+      'detail': 'Raging wildfire off I-90',
+      'time':   '2:06PM'
+    });
+    FirebaseFirestore.instance.collection('fire-updates').doc().set({
+      'name': 'Jane Doe',
+      'phone-number': '2130000000',
+      'location': 'California',
+      'detail': 'Wildfire caused by campfire.',
+      'time':   '2:13PM'
+    });
+
+  
+  }
+
+
+void updateData(){
+    FirebaseFirestore.instance.collection('fire-updates').doc().update({
+      'detail': 'Raging wildfire off I-90. Notifiied authorities. DNR responded.'
+    });
+  }
+
+  void deleteData(){
+    FirebaseFirestore.instance.collection('fire-updates').doc().delete();
+  }
