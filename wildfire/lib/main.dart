6359759571f5193df68d3d3e000351432ac54d4e
RollieDev <rolandoaniel@gmail.com>
2021-07-03 19:31:15 -0700

    Second Commit. Needs Review

--- wildfire/lib/main.dart
+++ wildfire/lib/main.dart
@@ -2,11 +2,10 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(WildfireApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -51,9 +50,9 @@ class _MyHomePageState extends State<MyHomePage> {
                   print("update ${element.data()}");
                 });
 
-                     return Container();
-            },
-          )
+                return Container();
+              },
+            )
           ],
         ),
       ), // This trailing comma makes auto-formatting nicer for build methods.
@@ -61,84 +60,63 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
- @override
-  _WildfireAppState createState() => _WildfireAppState();
+abstract class Wildfire {
+  late String _name, _phoneNumber, _location, _detail, _time;
 
-class _WildfireAppState extends State<WildfireApp> {
-  late String name, phoneNumber, location, detail, time;
+  CollectionReference fireUpdate =
+      FirebaseFirestore.instance.collection('fire-updates');
 
+  String get name => _name;
 
-    CollectionReference fireUpdate = FirebaseFirestore.instance.collection('fire-updates');
+  String get phoneNumber => _phoneNumber;
 
+  String get location => _location;
 
-  getName(name) {
-    this.name = name;
-  }
+  String get detail => _detail;
 
-  getPhoneNumber(phone-number) {
-    this.phoneNumber = phoneNumber;
-  }
+  String get time => _time;
+}
 
-  getLocation(location) {
-    this.location = location;
-  }
-
-  getDetail(detail) {
-    this.detail = detail;
-  }
-
-  getTime(time) {
-    this.time = time;
-  }
-
+class Report extends Wildfire {
   
-void readData(){
+  
+  void readData() {
     FirebaseFirestore.instance.collection("fire-updates").doc().get();
-      print("DATA ['name']}");
-      print("DATA ['phone-number']}");
-      print("DATA ['location']}");
-      print("DATA ['detail']}");
-      print("DATA ['time']}");
-    }
- }
-
-   Map<String,dynamic> toJson(){
-      return{
-      "name": name,
-      "phone-number": phoneNumber,
-      "location": location,
-      "detail": detail,
-      "time": time,
-    };
-  
+    print("$name ['name']}");
+    print("$phoneNumber ['phone-number']}");
+    print("$location ['location']}");
+    print("$detail ['detail']}");
+    print("$time ['time']}");
   }
+}
 
-void createData(){
-    FirebaseFirestore.instance.collection('fire-updates').doc().set({
-      'name': 'John Doe',
-      'phone-number': '2060000000',
-      'location': 'Northwest',
-      'detail': 'Raging wildfire off I-90',
-      'time':   '2:06PM'
-    });
-    FirebaseFirestore.instance.collection('fire-updates').doc().set({
-      'name': 'Jane Doe',
-      'phone-number': '2130000000',
-      'location': 'California',
-      'detail': 'Wildfire caused by campfire.',
-      'time':   '2:13PM'
-    });
+Query<Map<String, dynamic>> query =
+    FirebaseFirestore.instance.collection("fire-updates");
+final Stream<QuerySnapshot<Map<String, dynamic>>> snapshots = query.snapshots();
 
-  
-  }
+void createData() {
+  FirebaseFirestore.instance.collection('fire-updates').doc().set({
+    'name': 'John Doe',
+    'phone-number': '2060000000',
+    'location': 'Northwest',
+    'detail': 'Raging wildfire off I-90',
+    'time': '2:06PM'
+  });
+  FirebaseFirestore.instance.collection('fire-updates').doc().set({
+    'name': 'Jane Doe',
+    'phone-number': '2130000000',
+    'location': 'California',
+    'detail': 'Wildfire caused by campfire.',
+    'time': '2:13PM'
+  });
+}
 
+void updateData() {
+  FirebaseFirestore.instance.collection('fire-updates').doc().update({
+    'detail': 'Raging wildfire off I-90. Notifiied authorities. DNR responded.'
+  });
+}
 
-void updateData(){
-    FirebaseFirestore.instance.collection('fire-updates').doc().update({
-      'detail': 'Raging wildfire off I-90. Notifiied authorities. DNR responded.'
-    });
-  }
-
-  void deleteData(){
-    FirebaseFirestore.instance.collection('fire-updates').doc().delete();
-  }
+void deleteData() {
+  FirebaseFirestore.instance.collection('fire-updates').doc().delete();
+}
